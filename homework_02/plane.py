03fdfe566919aeac4b2d385eb19e553f99f1f2cd
Aslanov Ruslan <aslanov@speechpro.com>
2022-08-25 18:19:28 +0300

    2 functions added to plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -2,6 +2,7 @@
 создайте класс `Plane`, наследник `Vehicle`
 """
 from base import Vehicle
+import exceptions
 
 
 class Plane(Vehicle):
@@ -12,3 +13,14 @@ class Plane(Vehicle):
         super().__init__(weight, fuel, fuel_consumption)
         self.cargo = cargo
         self.max_cargo = max_cargo
+
+    def load_cargo(self, weight):
+        if (weight + self.cargo) <= self.cargo:
+            self.cargo += weight
+        else:
+            raise exceptions.CargoOverload()
+
+    def remove_all_cargo(self):
+        tmp = self.cargo
+        self.cargo = 0
+        return tmp
