d30d706ed6e6d1e75a6386bfd971d5025cd0d6a3
Aslanov Ruslan <aslanov@speechpro.com>
2022-09-03 20:48:50 +0300

    modules import fixed

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,15 +1,15 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
-from base import Vehicle
-import exceptions
+from homework_02.base import Vehicle
+from homework_02 import exceptions
 
 
 class Plane(Vehicle):
     cargo = 20
     max_cargo = 20
 
-    def __init__(self, weight, fuel, fuel_consumption, cargo, max_cargo):
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo, cargo):
         super().__init__(weight, fuel, fuel_consumption)
         self.cargo = cargo
         self.max_cargo = max_cargo
